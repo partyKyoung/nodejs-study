c10554286491aaccf29dedc72922d3613697bd1e
Youkyoung Park <kyoungah@kyoungah.com>
2018-07-27 01:37:10 +0900

    webpack 설정 끝

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,29 @@
 const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   target: 'node',
+  externals: [nodeExternals()],
   entry: {
     server: './src/server.js'
+  },
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      }
+    ]
   }
 };
